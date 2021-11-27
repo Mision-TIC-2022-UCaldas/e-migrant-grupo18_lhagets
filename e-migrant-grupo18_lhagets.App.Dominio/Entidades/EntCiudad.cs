27c56feb1c8f2867ad72dc5dc87c6ad6a5c37ac7
Nachog78 <nachog_bohorquez@yahoo.com>
2021-11-27 00:38:13 -0500

    Commit First Entity

---
+++ e-migrant-grupo18_lhagets.App.Dominio/Entidades/EntCiudad.cs
@@ -0,0 +1,8 @@
+using System;
+namespace e_migrant_grupo18_lhagets.App.Dominio{
+    public class EntCiudad{
+        public int cod_ciudad { get; set; }
+        public string ciudad { get; set; }
+            
+    }
+}
---
+++ e-migrant-grupo18_lhagets.App.Dominio/Entidades/EntPaises.cs
@@ -0,0 +1,8 @@
+using System;
+namespace e_migrant_grupo18_lhagets.App.Dominio{
+    public class EntPaises{
+        public int cod_pais { get; set; }
+        public string pais_origen { get; set; }
+            
+    }
+}
